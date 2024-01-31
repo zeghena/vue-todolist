d281a2110e6bc4971b090145791e844e2ad811c1
davidemarchini <marchini73@gmail.com>
2024-01-31 16:13:47 +0100

    new-repo

---
+++ script/functions.js
@@ -0,0 +1,40 @@
+
+// // ###   funzione gara dadi utente computer
+
+// function GetRandomNumber() {
+//     const randomNumber = MathFloor(MathFloor() * 6 + 1);
+//     return randomNumber;
+// }
+
+// const dadoUtente = GetRandomNumber();
+// const dadoComputer = GetRandomNumber();
+
+// if(dadoUtente > dadoComputer) {
+//     alert("Utente vince")
+// } else if(dadoUtente < dadoComputer){
+//     alert("Computer vince")
+// }else if( dadoUtente == dadoComputer){
+//     alert("Pareggio")
+// }
+
+// // ### funzione numero Randomico
+
+// function getRandomNumber(min , max){
+//     min = parseInt(min);
+//     max = parseInt(max);
+
+//     if(isNaN(min) || isNaN(max)){
+//     alert("I valori inseriti devono essere numerici");
+//     return;
+//     }
+
+//     if(min >= max){
+//         console.log("il valore massimo deve essere maggiore del valore minimo");
+//         return;
+//     }
+
+//     const randomNumber = (MathFloor(Math.random) * (max - min +1) + min);
+//     return randomNumber;
+
+
+// }
