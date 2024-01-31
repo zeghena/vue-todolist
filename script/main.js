0de2327c38c1f8119791d2b1af53112ba043741b
davidemarchini <marchini73@gmail.com>
2024-01-31 20:03:03 +0100

    splice

--- script/main.js
+++ script/main.js
@@ -1,35 +1,49 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-        
-        products: [{
-            prop1: 'Pane',
-            booleano2: true 
-          },
-          {
-            prop1: 'Latte',
-            booleano2: true 
-          },
-          {
-            prop1: 'Formaggio',
-            booleano2: true 
-          },
-          {
-            prop1: 'Verdura',
-            booleano2: true 
-          },
-          {
-            prop1: 'Carne',
-            booleano2: true 
-          },
-          
-          {
-            prop1: 'Acqua',
-            booleano2: true 
-          },
-        ]
-    }
-  }
-}).mount('#app')
+      products: [
+        {
+          prop1: "Pane",
+          booleano2: false,
+        },
+        {
+          prop1: "Latte",
+          booleano2: true,
+        },
+        {
+          prop1: "Formaggio",
+          booleano2: true,
+        },
+        {
+          prop1: "Verdura",
+          booleano2: true,
+        },
+        {
+          prop1: "Carne",
+          booleano2: true,
+        },
+
+        {
+          prop1: "Acqua",
+          booleano2: true,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    printHello() {
+      console.log("helloVue");
+    },
+    deleteP(id) {
+      console.log(this.products);
+      this.products.splice(id, 1);
+    },
+  },
+
+  mounted() {
+    this.printHello();
+  },
+}).mount("#app");
